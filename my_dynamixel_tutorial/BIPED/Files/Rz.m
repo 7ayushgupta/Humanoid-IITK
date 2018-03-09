4add73af4dcde7055172cc7d11b3d55d82666c5e
Ayush Gupta <7ayushgupta@gmail.com>
2018-03-09 20:47:54 +0530

    Added Simulation files

---
+++ my_dynamixel_tutorial/BIPED/Files/Rz.m
@@ -0,0 +1,3 @@
+function r = Rz(theta)
+    r = [cos(theta) sin(theta) 0 0;-sin(theta) cos(theta) 0 0;0 0 1 0;0 0 0 1];
+end
--- my_dynamixel_tutorial/trajectory_client.py
+++ my_dynamixel_tutorial/trajectory_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import roslib
 roslib.load_manifest('my_dynamixel_tutorial')
+import time
 
 import rospy
 import actionlib
@@ -29,20 +30,23 @@ class Joint:
             goal.trajectory.joint_names = ['motor8', 'motor9','motor10','motor11','motor12','motor13','motor14','motor15','motor16','motor17']
             point = JointTrajectoryPoint()
             point.positions = angles
-            point.time_from_start = rospy.Duration(3)                   
+            point.time_from_start = rospy.Duration(1.5)                   
             goal.trajectory.points.append(point)
             self.jta.send_goal_and_wait(goal)
               
 
 def main():
-	arm = Joint('f_arm')
-	#arm.move_joint([0])
-	arm.move_joint([3.1,2.49,3.41,0.22,-3.14,2.88,0.30,3.67,2.6,3.01])
-	arm.move_joint([2.8,2.49,3.41,0.22,-3.14,2.88,0.30,3.67,2.6,3.01])
-	arm.move_joint([2.8,2.49,3.41,0.22,-3.14,2.28,0.30,3.67,2.6,3.01])
-	arm.move_joint([3.1,2.49,3.41,0.22,-3.14,2.28,0.30,3.25,2.6,3.01])
-	arm.move_joint([3.1,2.63,3.75,0.22,-3.14,2.28,0.30,3.25,2.6,3.01])
-print("Hello1")
+    arm = Joint('f_arm') 	#arm.move_joint([0])
+    arm.move_joint([3.1,2.49,3.41,0.22,-3.14,2.88,0.30,3.67,2.6,3.01])
+    print("Step1 completed")
+    arm.move_joint([2.8,2.49,3.41,0.22,-3.14,2.88,0.30,3.67,2.6,3.01])
+    print("Step2 completed")
+    arm.move_joint([2.8,2.49,3.41,0.22,-3.14,2.28,0.30,3.67,2.6,3.01])
+    print("Step3 completed")
+    arm.move_joint([3.1,2.49,3.41,0.22,-3.14,2.28,0.30,3.25,2.6,3.01])
+    print("Step4 completed")
+    arm.move_joint([3.1,2.63,3.75,0.22,-3.14,2.28,0.30,3.25,2.6,3.01])
+    print("Exit")
                         
 if __name__ == '__main__':
 	rospy.init_node('trajectory_client')
